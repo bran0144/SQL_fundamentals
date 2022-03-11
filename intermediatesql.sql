097a0d56e2457d971167ff35a9df1c7cd9218921
KatieGott <Katie.Gott@target.com>
2022-03-11 14:39:03 -0600

    working on correlated queries

--- intermediatesql.sql
+++ intermediatesql.sql
@@ -516,3 +516,95 @@ ON l.country_id = m.country_id
 WHERE m.season = '2013/2014'
 GROUP BY l.name;
 
+-- Formatting your queries - 
+-- Line up SELECT, FROM, WHERE, GROUP BY
+-- Annotate your queries with comments
+/* multiline like this
+*/
+-- indent your queries
+-- Check out Holeywell style guide
+-- Subqueries can take a performance hit
+-- Make sure to properly filter in every subquery and main query
+
+-- Exercises:
+
+SELECT 
+	-- Select the stage and average goals for each stage
+	m.stage,
+    ROUND(AVG(m.home_goal + m.away_goal),2) AS avg_goals,
+    -- Select the average overall goals for the 2012/2013 season
+    ROUND((SELECT AVG(home_goal + away_goal) 
+           FROM match 
+           WHERE season = '2012/2013'),2) AS overall
+FROM match AS m
+-- Filter for the 2012/2013 season
+WHERE m.season = '2012/2013'
+-- Group by stage
+GROUP BY m.stage;
+
+SELECT 
+	-- Select the stage and average goals from the subquery
+	s.stage,
+	ROUND(s.avg_goals,2) AS avg_goals
+FROM 
+	-- Select the stage and average goals in 2012/2013
+	(SELECT
+		 stage,
+         AVG(home_goal + away_goal) AS avg_goals
+	 FROM match
+	 WHERE season = '2012/2013'
+	 GROUP BY stage) AS s
+WHERE 
+	-- Filter the main query using the subquery
+	s.avg_goals > (SELECT AVG(home_goal + away_goal) 
+                    FROM match WHERE season = '2012/2013');
+
+SELECT 
+	-- Select the stage and average goals from s
+	s.stage,
+    ROUND(s.avg_goals,2) AS avg_goal,
+    -- Select the overall average for 2012/2013
+    (SELECT AVG(home_goal + away_goal) FROM match WHERE season = '2012/2013') AS overall_avg
+FROM 
+	-- Select the stage and average goals in 2012/2013 from match
+	(SELECT
+		 stage,
+         AVG(home_goal + away_goal) AS avg_goals
+	 FROM match
+	 WHERE season = '2012/2013'
+	 GROUP BY stage) AS s
+WHERE 
+	-- Filter the main query using the subquery
+	s.avg_goals > (SELECT AVG(home_goal + away_goal) 
+                    FROM match WHERE season = '2012/2013');
+
+-- Correlated subqueries
+-- Use values from outer query to generate a result
+-- re-run for every row generated in the final data set
+-- Used for advanced joining, filtering, and evaluating data
+-- Simple vs correletaed subqueries
+-- Simple - 
+        -- can be run independently
+        -- evaluated once in the whole 
+-- Correlated Subquery
+        -- dependent on the main query
+        -- evaluated in loops (signifcantly slows down query runtime)
+
+SELECT
+        c.name AS country,
+        AVG(m.home_goal + m.away_goal) AS avg_goals
+FROM country AS c
+LEFT JOIN match AS m 
+ON c.id = m.country_id
+GROUP BY country;
+
+-- Instead of a join, do it as a corelated query:
+SELECT
+        c.name AS country,
+        (SELECT
+                AVG(m.home_goal + m.away_goal) 
+                FROM match AS m 
+                WHERE m.country_id = c.id)
+                AS avg_goals
+FROM country AS c
+GROUP BY country;
