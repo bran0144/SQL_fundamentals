1693c30151ea4d8071e3cf6335c6594c579198d5
KatieGott <Katie.Gott@target.com>
2022-03-31 12:55:00 -0500

    completed SQL Fundamentals Track

--- sqlfunctions.sql
+++ sqlfunctions.sql
@@ -612,3 +612,65 @@ FROM pg_available_extensions;
 SELECT extname
 FROM pg_extension;
 
+-- to enable an extension
+CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
+
+SELECT levenshtein('GUMBO', 'GAMBOL');
+-- calculates # of edits for strings to be a perfect match
+-- to compare to two strings using tri gram matching
+SELECT similarity('GUMBO', 'GAMBOL');
+
+-- Exercises:
+
+-- Enable the pg_trgm extension
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+
+-- Select all rows extensions
+SELECT * 
+FROM pg_extension;
+
+-- Select the title and description columns
+SELECT 
+  title, 
+  description, 
+  -- Calculate the similarity
+  similarity(title, description)
+FROM 
+  film;
+
+-- Select the title and description columns
+SELECT  
+  title, 
+  description, 
+  -- Calculate the levenshtein distance
+  levenshtein(title, 'JET NEIGHBOR') AS distance
+FROM 
+  film
+ORDER BY 3;
+
+-- Select the title and description columns
+SELECT  
+  title, 
+  description 
+FROM 
+  film
+WHERE 
+  -- Match "Astounding Drama" in the description
+  to_tsvector(description) @@ 
+  to_tsquery('Astounding & Drama');
+
+SELECT 
+  title, 
+  description, 
+  -- Calculate the similarity
+  similarity(description, 'Astounding Drama') 
+FROM 
+  film 
+WHERE 
+  to_tsvector(description) @@ 
+  to_tsquery('Astounding & Drama') 
+ORDER BY 
+	similarity(description, 'Astounding Drama') DESC;
+
+
+
